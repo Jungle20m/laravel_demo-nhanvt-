0ee401985b7f3714a5df30ce317343ef67be2e4a
Vietanh96 <vietanhd14cn7@gmail.com>
2020-08-06 06:42:18 +0700

    UI form

---
+++ resources/views/demo/index.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="css/demo.css" rel="stylesheet" type="text/css">
+
+
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-lg-12 mx-auto bg-white rounded shadow">
+                    <!-- Fixed header table-->
+                    <div class="table-responsive">
+                        <table class="table table-fixed">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th scope="col" class="col-2">#</th>
+                                    <th scope="col" class="col-4">Key</th>
+                                    <th scope="col" class="col-6">Value</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row" class="col-2">1</th>
+                                    <td class="col-4">Mark</td>
+                                    <td class="col-6">Otto</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" class="col-2">2</th>
+                                    <td class="col-4">Mark</td>
+                                    <td class="col-6">Otto</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" class="col-2">3</th>
+                                    <td class="col-4">Mark</td>
+                                    <td class="col-6">Otto</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" class="col-2">4</th>
+                                    <td class="col-4">Mark</td>
+                                    <td class="col-6">Otto</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" class="col-2">5</th>
+                                    <td class="col-4">Mark</td>
+                                    <td class="col-6">Otto</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" class="col-2">7</th>
+                                    <td class="col-4">Mark</td>
+                                    <td class="col-6">Otto</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" class="col-2">8</th>
+                                    <td class="col-4">Mark</td>
+                                    <td class="col-6">Otto</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row" class="col-2">9</th>
+                                    <td class="col-4">Mark</td>
+                                    <td class="col-6">Otto</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <form action="">
+                            <div class="form-group pt-4">
+                                <label for="key"><strong>Key</strong></label>
+                                <input type="text" name="key" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="value"><strong>Value</strong></label>
+                                <input type="text" name="value" class="form-control">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Add</button>
+                            <button type="submit" class="btn btn-secondary">Edit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</html>
